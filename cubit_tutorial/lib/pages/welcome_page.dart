21fd76be5936e5095f3ba57f35cac56c666e8d21
Sho Emoto <dd2d00seg1@gmail.com>
2022-01-04 14:08:44 +0900

    WelcomePageを表示

--- cubit_tutorial/lib/pages/welcome_page.dart
+++ cubit_tutorial/lib/pages/welcome_page.dart
@@ -8,8 +8,32 @@ class WelcomePage extends StatefulWidget {
 }
 
 class _WelcomePageState extends State<WelcomePage> {
+  final images = [
+    'welcome-one.png',
+    'welcome-two.png',
+    'welcome-three.png',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    // 上下スワイプをPageViewで作る
+    return Scaffold(
+      body: PageView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: images.length,
+        itemBuilder: (_, index) {
+          return Container(
+            width: double.maxFinite,
+            height: double.maxFinite,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/' + images[index]),
+                fit: BoxFit.cover
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
