2f741ab2cc22a7c0450b3cb4df43ae64bea6caec
Sho Emoto <dd2d00seg1@gmail.com>
2022-01-05 17:51:10 +0900

    HomePageのレイアウトを組む

--- cubit_tutorial/lib/pages/bottom_navigation/home_page.dart
+++ cubit_tutorial/lib/pages/bottom_navigation/home_page.dart
@@ -1,10 +1,219 @@
+import 'package:cubit_tutorial/theme/colors.dart';
+import 'package:cubit_tutorial/widgets/app_large_text.dart';
+import 'package:cubit_tutorial/widgets/app_text.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+// TabBarの状態変化を同期する
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    // vsyncで自信を指定するためにここでインスタンス化
+    _tabController = TabController(length: 3, vsync: this);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('HomePage'));
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Top Bar
+          Container(
+            padding: const EdgeInsets.only(
+              left: 20,
+              top: 70,
+              right: 20,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Icon(
+                  Icons.menu,
+                  size: 30,
+                  color: Colors.black54,
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey.withOpacity(0.5),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 40),
+          // Discover Text
+          Container(
+            padding: const EdgeInsets.only(left: 20),
+            child: const AppLargeText(text: 'Discover'),
+          ),
+          const SizedBox(height: 30),
+          // TabBar
+          Align(
+            alignment: Alignment.centerLeft,
+            child: TabBar(
+              controller: _tabController,
+              labelPadding: const EdgeInsets.only(left: 20, right: 20),
+              labelColor: Colors.black,
+              unselectedLabelColor: Colors.grey,
+              isScrollable: true,
+              indicatorSize: TabBarIndicatorSize.label,
+              // 自作Indicator
+              indicator: const CircleTabIndicator(
+                color: AppColors.mainColor,
+                radius: 4,
+              ),
+              tabs: const [
+                Tab(text: 'Places'),
+                Tab(text: 'Inspiration'),
+                Tab(text: 'Emotions'),
+              ],
+            ),
+          ),
+          // TabBarView
+          Container(
+            padding: const EdgeInsets.only(left: 20),
+            // サイズの指定が必要
+            width: double.maxFinite,
+            height: 300,
+            child: TabBarView(
+              controller: _tabController,
+              children: [
+                ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 3,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: const EdgeInsets.only(
+                        top: 10,
+                        right: 15,
+                      ),
+                      width: 200,
+                      height: 300,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.white,
+                        image: const DecorationImage(
+                          image: AssetImage('assets/images/mountain.jpeg'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                Text('There'),
+                Text('Bye'),
+              ],
+            ),
+          ),
+          const SizedBox(height: 30),
+          // Explore more Text
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                AppLargeText(
+                  text: 'Explore more',
+                  size: 22,
+                ),
+                AppText(
+                  text: 'See all',
+                  color: AppColors.textColor1,
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 10),
+          // Explore Category
+          Container(
+            width: double.maxFinite,
+            height: 100,
+            margin: const EdgeInsets.only(left: 20),
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 4,
+              itemBuilder: (_, index) {
+                return Container(
+                  margin: const EdgeInsets.only(right: 30),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 80,
+                        height: 80,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.white,
+                          image: const DecorationImage(
+                            image: AssetImage('assets/images/mountain.jpeg'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      const AppText(
+                        text: 'Kayaking',
+                        color: AppColors.textColor2,
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CircleTabIndicator extends Decoration {
+  final Color color;
+  final double radius;
+
+  const CircleTabIndicator({required this.color, required this.radius});
+
+  @override
+  BoxPainter createBoxPainter([VoidCallback? onChanged]) {
+    return CirclePainter(color: color, radius: radius);
+  }
+}
+
+class CirclePainter extends BoxPainter {
+  final Color color;
+  final double radius;
+
+  const CirclePainter({required this.color, required this.radius});
+
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    final paint = Paint();
+    paint.color = color;
+    paint.isAntiAlias = true;
+
+    // 位置調整
+    // NOTE: configuration(ImageConfiguration)がタップした領域の情報を持っている
+    final circleOffset = Offset(
+      configuration.size!.width / 2 - radius / 2,
+      configuration.size!.height - radius,
+    );
+
+    canvas.drawCircle(
+      offset + circleOffset,
+      radius,
+      paint,
+    );
   }
 }
