5098a17a35fcfaaa91c1f28cd8819c4bee5e12b2
Sho Emoto <dd2d00seg1@gmail.com>
2022-01-05 18:14:04 +0900

    HomePageの各パーツをメソッドに切り出し

--- cubit_tutorial/lib/pages/bottom_navigation/home_page.dart
+++ cubit_tutorial/lib/pages/bottom_navigation/home_page.dart
@@ -42,156 +42,180 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           // Top Bar
-          Container(
-            padding: const EdgeInsets.only(
-              left: 20,
-              top: 70,
-              right: 20,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Icon(
-                  Icons.menu,
-                  size: 30,
-                  color: Colors.black54,
-                ),
-                Container(
-                  width: 50,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.grey.withOpacity(0.5),
-                  ),
-                ),
-              ],
-            ),
-          ),
+          _topBar(),
           const SizedBox(height: 40),
           // Discover Text
-          Container(
-            padding: const EdgeInsets.only(left: 20),
-            child: const AppLargeText(text: 'Discover'),
-          ),
+          _discoverText(),
           const SizedBox(height: 30),
           // TabBar
-          Align(
-            alignment: Alignment.centerLeft,
-            child: TabBar(
-              controller: _tabController,
-              labelPadding: const EdgeInsets.only(left: 20, right: 20),
-              labelColor: Colors.black,
-              unselectedLabelColor: Colors.grey,
-              isScrollable: true,
-              indicatorSize: TabBarIndicatorSize.label,
-              // 自作Indicator
-              indicator: const CircleTabIndicator(
-                color: AppColors.mainColor,
-                radius: 4,
-              ),
-              tabs: const [
-                Tab(text: 'Places'),
-                Tab(text: 'Inspiration'),
-                Tab(text: 'Emotions'),
-              ],
-            ),
-          ),
+          _tabBar(),
           // TabBarView
-          Container(
-            padding: const EdgeInsets.only(left: 20),
-            // サイズの指定が必要
-            width: double.maxFinite,
-            height: 300,
-            child: TabBarView(
-              controller: _tabController,
-              children: [
-                ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 3,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      margin: const EdgeInsets.only(
-                        top: 10,
-                        right: 15,
-                      ),
-                      width: 200,
-                      height: 300,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.white,
-                        image: const DecorationImage(
-                          image: AssetImage('assets/images/mountain.jpeg'),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    );
-                  },
-                ),
-                Text('There'),
-                Text('Bye'),
-              ],
-            ),
-          ),
+          _tabBarView(),
           const SizedBox(height: 30),
           // Explore more Text
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const [
-                AppLargeText(
-                  text: 'Explore more',
-                  size: 22,
-                ),
-                AppText(
-                  text: 'See all',
-                  color: AppColors.textColor1,
-                ),
-              ],
-            ),
-          ),
+          _exploreMoreText(),
           const SizedBox(height: 10),
           // Explore Category
+          _exploreCategory(),
+        ],
+      ),
+    );
+  }
+
+  Widget _topBar() {
+    return Container(
+      padding: const EdgeInsets.only(
+        left: 20,
+        top: 70,
+        right: 20,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Icon(
+            Icons.menu,
+            size: 30,
+            color: Colors.black54,
+          ),
           Container(
-            width: double.maxFinite,
-            height: 100,
-            margin: const EdgeInsets.only(left: 20),
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: 4,
-              itemBuilder: (_, index) {
-                final image = imagesMap[index];
-                return Container(
-                  margin: const EdgeInsets.only(right: 30),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: 80,
-                        height: 80,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.white,
-                          image: DecorationImage(
-                            image: AssetImage(
-                              'assets/images/' +
-                                  imagesMap.values.elementAt(index),
-                            ),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      AppText(
-                        text: imagesMap.keys.elementAt(index),
-                        color: AppColors.textColor2,
-                      ),
-                    ],
-                  ),
-                );
-              },
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.grey.withOpacity(0.5),
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _discoverText() {
+    return Container(
+      padding: const EdgeInsets.only(left: 20),
+      child: const AppLargeText(text: 'Discover'),
+    );
+  }
+
+  Widget _tabBar() {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: TabBar(
+        controller: _tabController,
+        labelPadding: const EdgeInsets.only(left: 20, right: 20),
+        labelColor: Colors.black,
+        unselectedLabelColor: Colors.grey,
+        isScrollable: true,
+        indicatorSize: TabBarIndicatorSize.label,
+        // 自作Indicator
+        indicator: const CircleTabIndicator(
+          color: AppColors.mainColor,
+          radius: 4,
+        ),
+        tabs: const [
+          Tab(text: 'Places'),
+          Tab(text: 'Inspiration'),
+          Tab(text: 'Emotions'),
+        ],
+      ),
+    );
+  }
+
+  Widget _tabBarView() {
+    return Container(
+      padding: const EdgeInsets.only(left: 20),
+      // サイズの指定が必要
+      width: double.maxFinite,
+      height: 300,
+      child: TabBarView(
+        controller: _tabController,
+        children: [
+          ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: 3,
+            itemBuilder: (context, index) {
+              return Container(
+                margin: const EdgeInsets.only(
+                  top: 10,
+                  right: 15,
+                ),
+                width: 200,
+                height: 300,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.white,
+                  image: const DecorationImage(
+                    image: AssetImage('assets/images/mountain.jpeg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              );
+            },
+          ),
+          Text('There'),
+          Text('Bye'),
+        ],
+      ),
+    );
+  }
+
+  Widget _exploreMoreText() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: const [
+          AppLargeText(
+            text: 'Explore more',
+            size: 22,
+          ),
+          AppText(
+            text: 'See all',
+            color: AppColors.textColor1,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _exploreCategory() {
+    return Container(
+      width: double.maxFinite,
+      height: 100,
+      margin: const EdgeInsets.only(left: 20),
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 4,
+        itemBuilder: (_, index) {
+          final image = imagesMap[index];
+          return Container(
+            margin: const EdgeInsets.only(right: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 80,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                    image: DecorationImage(
+                      image: AssetImage(
+                        'assets/images/' +
+                            imagesMap.values.elementAt(index),
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                AppText(
+                  text: imagesMap.keys.elementAt(index),
+                  color: AppColors.textColor2,
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
